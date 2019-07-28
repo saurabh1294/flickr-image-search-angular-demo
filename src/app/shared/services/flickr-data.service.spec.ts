edc3c8987dec6acd93ef886b66e26227d543a750
Saurabh Gattani <saurabh.gattani@voicefoundry.com.au>
2019-07-28 23:03:16 +1000

    removed unwanted code

--- src/app/shared/services/flickr-data.service.spec.ts
+++ src/app/shared/services/flickr-data.service.spec.ts
@@ -36,13 +36,6 @@ describe('FlickrDataService', () => {
           useValue: { flickrDataService, getImages: ObserverThrowServiceStub }
         }
       ]
-      // providers: [
-      //   {
-      //     provide: FlickrDataService,
-      //     useClass: FlickrDataService,
-      //     useValue: { flickrDataService, getImages: ObserverThrowServiceStub }
-      //   }
-      // ]
     });
 
     flickrDataService = TestBed.get(FlickrDataService);
@@ -57,46 +50,9 @@ describe('FlickrDataService', () => {
     it('should fetch images from flickr feed', inject(
       [FlickrDataService, HttpTestingController, HttpClient],
       (backend: HttpTestingController, http: HttpClient, httpClient: HttpClient, httpJsonpModule: HttpClientJsonpModule) => {
-        // const flickrObservable = of(null);
-        // flickrDataService = new FlickrDataService(http);
-        // flickrDataService.getImages("Australia").subscribe(res=>console.log(res));
-        // const req = backend.expectOne(`https://api.flickr.com/services/feeds/photos_public.gne?
-        // &tags=Australia&format=json&jsoncallback=JSONP_CALLBACK`, 'JSONP_CALLBACK');
-        // expect(req.request.body).toEqual({})
-        // req.flush({data: 'test-data'})
-        // let result: any;
-        // flickrDataService.getImages("Australia").subscribe(data => (result = data), fail);
-        // backend.expectNone('https://api.flickr.com/services/feeds/photos_public.gne');
-        // expect(result).toBe(null);
-        // successHandler.and.callThrough();
-        // successHandler.and.returnValue(of({ data: 'test-data' }));
-
-        const dummyData = { data: 'test-data' };
-
-        // flickrDataService.getImages("Australia").subscribe(data => {
-        //   expect(data).toEqual(dummyData);
-        // });
-
-        // backend.expectNone(
-        //   req => req.method === 'GET' && req.url === 'https://api.flickr.com/services/feeds/photos_public.gne'
-        // );
-        // expect(result).toBe(null);
-
         successHandler.and.returnValue(Promise.resolve({ data: 'test-data' }));
-        // jasmine.createSpy('getImages').and.callThrough();
-        // flickrDataService.getImages('Australia').subscribe((response: any) => {
-        //   expect(response.key).toBe('value');
-        // });
-        // wait until all Promises are resolved
-        // tick();
         successHandler.and.callThrough();
         successHandler.and.returnValue(of({ data: 'test-data' }));
-        //   const req = httpMock.expectNone(request => request.url === `
-        // https://api.flickr.com/services/feeds/photos_public.gne?
-        //   &tags=Australia&format=json&jsoncallback=JSONP_CALLBACK
-        // `);
-        // expect(req.request.method).toBe('JSONP');
-        // req.flush(dummyData);
       }
     ));
   });
